79bde689605afbd04d89e8c137e459b1a297c48e
Luis Pineda JR <lspnd00@gmail.com>
2017-10-04 22:52:57 -0400

    added music

--- scripts/album.js
+++ scripts/album.js
@@ -1,7 +1,24 @@
 var setSong = function(songNumber){
+        if (currentSoundFile) {
+           currentSoundFile.stop();
+       }
           currentlyPlayingSongNumber = parseInt(songNumber);
           currentSongFromAlbum = currentAlbum.songs[songNumber - 1];
+          currentSoundFile = new buzz.sound(currentSongFromAlbum.audioUrl, {
+                  formats: [ 'mp3' ],
+                   preload: true
+              });
+
+    setVolume(currentVolume);
 };
+
+var setVolume = function(volume) {
+     if (currentSoundFile) {
+         currentSoundFile.setVolume(volume);
+     }
+ };
+
+
 var getSongNumberCell = function(number){
       return $('.song-item-number[data-song-number="' + number + '"]');
 };
@@ -30,13 +47,17 @@ var createSongRow = function(songNumber, songName, songLength) {
                      // Switch from Play -> Pause button to indicate new song is playing.
                      $(this).html(pauseButtonTemplate);
                      setSong(songNumber);
-                     updatePlayerBarSong();
+                     currentSoundFile.play();
             }else if (currentlyPlayingSongNumber === songNumber) {
-                     // Switch from Pause -> Play button to pause currently playing song.
-                     $(this).html(playButtonTemplate);
-                     $('.main-controls .play-pause').html(playerBarPlayButton);
-                     currentlyPlayingSongNumber = null;
-                     currentSongFromAlbum = null;
+              if (currentSoundFile.isPaused()) {
+                  $(this).html(pauseButtonTemplate);
+                  $('.main-controls .play-pause').html(playerBarPauseButton);
+                  currentSoundFile.play();
+                } else {
+                    $(this).html(playButtonTemplate);
+                    $('.main-controls .play-pause').html(playerBarPlayButton);
+                    currentSoundFile.pause();
+                  }
             }
      };
 
@@ -109,20 +130,37 @@ var playButtonTemplate = '<a class="album-song-button"><span class="ion-play"></
 var pauseButtonTemplate = '<a class="album-song-button"><span class="ion-pause"></span></a>';
 var playerBarPlayButton = '<span class="ion-play"></span>';
 var playerBarPauseButton = '<span class="ion-pause"></span>';
-
 // Store state of playing songs
  var currentAlbum = null;
  var currentlyPlayingSongNumber = null;
  var currentSongFromAlbum = null;
+ var currentSoundFile = null;
+ var currentVolume = 80;
 
  var $previousButton = $('.main-controls .previous');
  var $nextButton = $('.main-controls .next');
+ var $togglePlayFromPlayerBar  = $('.main-controls .play-pause');
 
 $(document).ready(function() {
           setCurrentAlbum(albumPicasso);
           $previousButton.click(previousSong);
           $nextButton.click(nextSong);
+          $togglePlayFromPlayerBar.click(clickHandler);
 });
+
+var togglePlayFromPlayerBar = function(){
+  if (currentSoundFile.isPaused()) {
+      $(this).html(pauseButtonTemplate);
+      $('.main-controls .play-pause').html(playerBarPauseButton);
+      currentSoundFile.play();
+    } else {
+        $(this).html(playButtonTemplate);
+        $('.main-controls .play-pause').html(playerBarPlayButton);
+        currentSoundFile.pause();
+      }
+}
+
+
 
     //   var albums = [albumPicasso, albumMarconi];
     //   var index = 1;
@@ -147,7 +185,8 @@ $(document).ready(function() {
         var lastSongNumber = currentlyPlayingSongNumber;
 
         // Set a new current song
-        currentlyPlayingSongNumber = currentSongIndex + 1;
+        setSong(currentSongIndex + 1);
+        currentSoundFile.play();
         currentSongFromAlbum = currentAlbum.songs[currentSongIndex];
 
         // Update the Player Bar information
@@ -174,7 +213,8 @@ var previousSong = function() {
     var lastSongNumber = currentlyPlayingSongNumber;
 
     // Set a new current song
-    currentlyPlayingSongNumber = currentSongIndex + 1;
+    setSong(currentSongIndex + 1);
+    currentSoundFile.play();
     currentSongFromAlbum = currentAlbum.songs[currentSongIndex];
 
     // Update the Player Bar information
